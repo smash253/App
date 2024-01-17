bf33d900ece02b8a9f248582a5838c00ed54de09
Darie-Nistor Nicolae <memedarie@gmail.com>
2024-01-17 21:18:31 +0100

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Darie Nistor Nicolae on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
